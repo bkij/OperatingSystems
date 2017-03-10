d47cdc233eceddeb169e632b1f1c91eb653fc632
Bartosz Kijanka <duelbart5@gmail.com>
2017-03-10 14:25:25 +0100

    Add two-way list declaration

--- zestaw1/contact_book.h
+++ zestaw1/contact_book.h
@@ -1,6 +1,31 @@
 #ifndef CONTACT_BOOK_H
 #define CONTACT_BOOK_H
 
+#include <stdbool.h>
 #include "contact_info.h"
 
+
+/*
+ * List data structure and functions
+ */
+
+typedef struct ContactNode {
+    ContactInfo contact_data;
+    ContactNode *prev;
+    ContactNode *next;
+} ContactNode;
+
+typedef struct ContactList {
+    ContactNode *first;
+    ContactNode *last;
+}
+
+void add_contact_info(ContactList *contact_list, ContactInfo *contact_info);
+bool remove_contact_info(ContactList *contact_list, ContactInfo *contact_info);
+ContactInfo *find_contact_info(ContactList *contact_list, ContactInfo *contact_info);
+void sort_contact_list(ContactList *contact_list, int (*comparator)(ContactInfo *left, ContactInfo *right));
+
+/*
+ * Tree data structure and functions
+ */
 #endif
