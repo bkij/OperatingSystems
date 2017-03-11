76a2942ac50da302b6228388c0491c9559e8d757
Bartosz Kijanka <duelbart5@gmail.com>
2017-03-11 13:05:43 +0100

    Add RBTree declarations and enums

--- zestaw1/contact_book.h
+++ zestaw1/contact_book.h
@@ -10,7 +10,7 @@
  */
 
 typedef struct ContactNode {
-    ContactInfo contact_data;
+    ContactInfo *contact_data;
     ContactNode *prev;
     ContactNode *next;
 } ContactNode;
@@ -29,4 +29,34 @@ void list_sort(ContactList *contact_list, int (*comparator)(ContactInfo *left, C
  * Tree data structure and functions
  */
 
+typedef enum {red, black} Color;
+
+/*
+ * ContactInfo poitner below is actually an
+ * array of size data_size. That's because
+ * I allow multiple values for the same key.
+ * Those values are stored in a single node as this array.
+ */
+typedef struct RBNode {
+    RBNode *parent;
+    RBNode *left;
+    RBNode *right;
+    Color color;
+    ContactInfo *contact_data;
+    int data_size;
+} RBNode;
+
+typedef struct ContactTree {
+    RBNode root;
+}
+
+void tree_add(ContactTree *contact_tree, ContactInfo *contact_info);
+bool tree_remove(ContactTree *contact_tree, ContactInfo *contact_info);
+ContactInfo *tree_find(ContactTree *contact_tree, ContactInfo *contact_info);
+
+/*
+ * Using an enum to signal the new key on which to rebuild the tree
+ */
+typedef enum {surname_rebuild, birthdate_rebuild, mail_rebuild, phone_rebuild} rebuild_case; 
+void tree_rebuild(ContactTree *contact_tree, rebuild_case how);
 #endif
