6c72f30028e5c3bc4e4d608b3ef94162fd2f77a7
Bartosz Kijanka <duelbart5@gmail.com>
2017-03-15 13:04:35 +0100

    Add file data parsing

--- zestaw2/io_benchmark/io_benchmark.c
+++ zestaw2/io_benchmark/io_benchmark.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 #include "file_manip.h"
 
-void parse_args(int argc, char **argv, int *num_records, int *record_size, char **filename, Mode *mode, FunctionType *type);
+bool parse_args(int argc, char **argv, int *num_records, int *record_size, char **filename, Mode *mode, FunctionType *type);
+bool parse_record_options(char **argv, int *num_records, int *record_size, char **filename);
+bool parse_mode_and_type(char **argv, Mode *mode, FunctionType *type);
+void print_usage();
 
 int main(int argc, char **argv)
 {
@@ -10,4 +17,53 @@ int main(int argc, char **argv)
     Mode mode;
     FunctionType type;
     return 0;
+}
+
+bool parse_args(int argc, char **argv, int *num_records, int *record_size, char **filename, Mode *mode, FunctionType *type)
+{
+    if(argc < 5) {
+        print_usage();
+        // TODO: Add perror
+        return false;
+    }
+    if(!strcmp(argv[1], "generate")) {
+        return parse_record_options(argv + 2, num_records, record_size, filename);
+    }
+    else {
+        return parse_mode_and_type(argv, mode, type) && parse_record_options(argv + 3, num_records, record_size, filename);
+    }
+}
+
+bool parse_record_options(char **argv, int *num_records, int *record_size, char **filename)
+{
+    if(argv[0] == NULL) {
+        // TODO: Think if this is neccessary
+        print_usage();
+        return false;
+    }
+    *filename = argv[0];
+
+    if(argv[1] == NULL) {
+        print_usage();
+        return false;
+    }
+    long tmp = strtol(argv[1], NULL, 10);
+    if(errno == ERANGE || tmp <= 0 || tmp >= INT_MAX) {
+        print_usage();
+        return false;
+    }
+    *num_records = (int)tmp;
+
+    tmp = strtol(argv[2], NULL, 10);
+    if(errno == ERANGE || tmp <= 0 || tmp >= INT_MAX) {
+        print_usage();
+        return false;
+    }
+    *record_size = (int)tmp;
+
+    return true;
+}
+
+void print_usage()
+{
 }
