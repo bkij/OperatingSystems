8d0f36c22ca91043629658da1a6049081d2f345c
Bartosz Kijanka <duelbart5@gmail.com>
2017-03-15 12:43:22 +0100

    Add skelethon code for 1st task

---
+++ zestaw2/io_benchmark/file_manip.h
@@ -0,0 +1,7 @@
+#ifndef FILE_MANIP_H
+#define FILE_MANIP_H
+
+typedef enum {generate, shuffle, sort} Mode;
+typedef enum {sys, lib} FunctionType;
+
+#endif
---
+++ zestaw2/io_benchmark/io_benchmark.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "file_manip.h"
+
+void parse_args(int argc, char **argv, int *num_records, int *record_size, char **filename, Mode *mode, FunctionType *type);
+
+int main(int argc, char **argv)
+{
+    int num_records, record_size;
+    char *filename;
+    Mode mode;
+    FunctionType type;
+    return 0;
+}
