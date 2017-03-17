150043430ea19f393b15f5f7831f29803c798ba8
Bartosz Kijanka <duelbart5@gmail.com>
2017-03-17 12:28:59 +0100

    Add bubble sort

--- zestaw2/io_benchmark/file_manip.c
+++ zestaw2/io_benchmark/file_manip.c
@@ -7,7 +7,12 @@
 #include <stdbool.h>
 #include <malloc.h>
 #include <time.h>
+#include <stdlib.h>
 #include "file_manip.h"
+
+/*
+ * HELPER FUNCTIONS
+ */
 
 void print_err_seek(char *filename)
 {
@@ -31,7 +36,7 @@ void print_err_close(char *filename)
 
 void print_err_open(char *filename)
 {
-    fptinf(stderr, "%s %s\n", "Error openging the file", filename);
+    fprintf(stderr, "%s %s\n", "Error openging the file", filename);
 }
 
 int execute(char *filename, int num_records, int record_size, Command command, FunctionType type)
@@ -49,22 +54,8 @@ int execute(char *filename, int num_records, int record_size, Command command, F
     }
 }
 
-int swap_records_sys(int fd, char *buf_left, char *buf_right, int i, int j)
+int swap_records_sys(int file_fd, char *buf_left, char *buf_right, int i, int j, int record_size)
 {
-        // READ
-        if(lseek(file_fd, i * record_size, SEEK_SET) != i * record_size) {
-            return -1;
-        }
-        if(read(file_fd, buf_left, record_size) != record_size) {
-            return -1;
-        }
-        if(lseek(file_fd, j * record_size, SEEK_SET) != j * record_size) {
-            return -1;
-        }
-        if(read(file_fd, buf_right, record_size) != record_size) {
-            return -1;
-        }
-        // WRITE
         if(lseek(file_fd, j * record_size, SEEK_SET) != j * record_size) {
             return -1;
         }
@@ -74,32 +65,14 @@ int swap_records_sys(int fd, char *buf_left, char *buf_right, int i, int j)
         if(lseek(file_fd, i * record_size, SEEK_SET) != i * record_size) {
             return -1;
         }
-        if(wrte(file_fd, buf_right, record_size) != record_size) {
+        if(write(file_fd, buf_right, record_size) != record_size) {
             return -1;
         }
         return 0;
 }
 
-int swap_records_lib(FILE *file, char *buf_left, char *buf_right, int i, int j)
+int swap_records_lib(FILE *file, char *buf_left, char *buf_right, int i, int j, int record_size, char *filename)
 {
-        //READ
-        if(fseek(file, i * record_size, SEEK_SET) != i * record_size) {
-            print_err_seek(filename);
-            return -1;
-        }
-        if(fread(buf_left, 1, record_size, file) != record_size) {
-            print_err_read(filename);
-            return -1;
-        }
-        if(fseek(file, j * record_size, SEEK_SET) != j * record_size) {
-            print_err_seek(filename);
-            return -1;
-        }
-        if(fread(buf_right, 1, record_size, file) != record_size) {
-            print_err_read(filename);
-            return -1;
-        }
-        //WRITE
         if(fseek(file, j * record_size, SEEK_SET) != j * record_size) {
             print_err_seek(filename);
             return -1;
@@ -118,6 +91,21 @@ int swap_records_lib(FILE *file, char *buf_left, char *buf_right, int i, int j)
         }
         return 0;
 }
+
+int cmp_records(char *record_left, char *record_right)
+{
+    // Compare first byte
+    if((unsigned char)(*record_left) == (unsigned char)(*record_right)) {
+        return 0;
+    }
+    else {
+        return (unsigned char)(*record_left) > (unsigned char)(*record_right);
+    }
+}
+
+/*
+ * API and related functions
+ */
 
 int generate_sys(char *filename, int num_records, int record_size)
 {
@@ -256,7 +244,21 @@ int shuffle_sys(char *filename, int num_records, int record_size)
 
     for(int i = 0; i < num_records - 1; i++) {
         int j = rand() % num_records;
-        if(swap_records_sys(file_fd, buf_left, buf_right, i, j) < 0) {
+        // READ
+        if(lseek(file_fd, i * record_size, SEEK_SET) != i * record_size) {
+            return -1;
+        }
+        if(read(file_fd, buf_left, record_size) != record_size) {
+            return -1;
+        }
+        if(lseek(file_fd, j * record_size, SEEK_SET) != j * record_size) {
+            return -1;
+        }
+        if(read(file_fd, buf_right, record_size) != record_size) {
+            return -1;
+        }
+        //WRITE
+        if(swap_records_sys(file_fd, buf_left, buf_right, i, j, record_size) < 0) {
             goto err_clean;
         }
     }
@@ -295,7 +297,25 @@ int shuffle_lib(char *filename, int num_records, int record_size)
     srand(time(NULL));
     for(int i = 0; i < num_records - 1; i++) {
         int j = rand() % num_records;
-        if(swap_records_lib(file, buf_left, buf_right, i, j) < 0) {
+        //READ
+       if(fseek(file, i * record_size, SEEK_SET) != i * record_size) {
+            print_err_seek(filename);
+            return -1;
+        }
+        if(fread(buf_left, 1, record_size, file) != record_size) {
+            print_err_read(filename);
+            return -1;
+        }
+        if(fseek(file, j * record_size, SEEK_SET) != j * record_size) {
+            print_err_seek(filename);
+            return -1;
+        }
+        if(fread(buf_right, 1, record_size, file) != record_size) {
+            print_err_read(filename);
+            return -1;
+        }
+        //WRITE
+        if(swap_records_lib(file, buf_left, buf_right, i, j, record_size, filename) < 0) {
             goto err_clean;
         }
     }
@@ -316,3 +336,130 @@ err_clean:
     return -1;
 }
 
+int shuffle(char *filename, int num_records, int record_size, FunctionType type)
+{
+    if(type == sys) {
+        return shuffle_sys(filename, num_records, record_size);
+    }
+    else {
+        return shuffle_lib(filename, num_records, record_size);
+    }
+}
+
+int sort_sys(char *filename, int num_records, int record_size)
+{
+    int file_fd;
+    bool cleanup_file = true;
+    char *buf_left = malloc(record_size);
+    char *buf_right = malloc(record_size);
+
+    file_fd = open(filename, O_RDWR);
+    if(file_fd < 0) {
+        goto err_clean;
+    }
+    cleanup_file = true;
+
+    for(int cnt = num_records; cnt > 1; cnt--) {
+       for(int i = 0; i < cnt - 1; i++) {
+           int j = i + 1;
+           if(lseek(file_fd, i * record_size, SEEK_SET) != i * record_size) {
+               return -1;
+           }
+           if(read(file_fd, buf_left, record_size) != record_size) {
+               return -1;
+           }
+           if(lseek(file_fd, j * record_size, SEEK_SET) != j * record_size) {
+               return -1;
+           }
+           if(read(file_fd, buf_right, record_size) != record_size) {
+               return -1;
+           }
+           if(cmp_records(buf_left, buf_right) > 0) {
+               if(swap_records_sys(file_fd, buf_left, buf_right, i, j, record_size)) {
+                   goto err_clean;
+               }
+           }
+       }
+    }
+
+    if(close(file_fd) < 0) {
+        perror("Error closing file");
+    }
+    free(buf_left);
+    free(buf_right);
+    return 0;
+err_clean:
+    perror("Error in function sort");
+    if(cleanup_file && close(file_fd) < 0) {
+        perror("Error closing file");
+    }
+    free(buf_left);
+    free(buf_right);
+    return -1;
+}
+
+int sort_lib(char *filename, int num_records, int record_size)
+{
+    FILE *file;
+    bool cleanup_file = false;
+    char *buf_left = malloc(record_size);
+    char *buf_right = malloc(record_size);
+
+    file = fopen(filename, "r+");
+    if(file == NULL) {
+        goto err_clean;
+    }
+    cleanup_file = true;
+
+    for(int cnt = num_records; cnt > 1; cnt--) {
+        for(int i = 0; i < cnt - 1; i++) {
+            int j = i + 1;
+            if(fseek(file, i * record_size, SEEK_SET) != i * record_size) {
+                print_err_seek(filename);
+                return -1;
+            }
+            if(fread(buf_left, 1, record_size, file) != record_size) {
+                print_err_read(filename);
+                return -1;
+            }
+            if(fseek(file, j * record_size, SEEK_SET) != j * record_size) {
+                print_err_seek(filename);
+                return -1;
+            }
+            if(fread(buf_right, 1, record_size, file) != record_size) {
+                print_err_read(filename);
+                return -1;
+            }
+            if(cmp_records(buf_left, buf_right) > 0) {
+                if(swap_records_lib(file, buf_left, buf_right, i, j, record_size, filename) < 0) {
+                    goto err_clean;
+                }
+            }
+        }
+    }
+
+    if(fclose(file) < 0) {
+        print_err_close(filename);
+    }
+    free(buf_left);
+    free(buf_right);
+    return 0;
+err_clean:
+    if(cleanup_file && fclose(file) < 0) {
+        print_err_close(filename);
+    }
+    free(buf_left);
+    free(buf_right);
+    return -1;
+}
+
+int sort(char *filename, int num_records, int record_size, FunctionType type)
+{
+    // Bubble sort
+    if(type == sys) {
+        return sort_sys(filename, num_records, record_size);
+    }
+    else {
+        return sort_lib(filename, num_records, record_size);
+    }
+}
